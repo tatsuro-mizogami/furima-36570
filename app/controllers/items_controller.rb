11ea87a4564d38b3938b4207df3f96b521f65f3c
溝上達朗 <mizogamitatsurou@mizokamitatsurounoiMac.local>
2021-08-28 10:33:11 +0900

    新規登録、ログイン機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,3 +11,4 @@ class ItemsController < ApplicationController
     
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+
+  has_many :items
+  has_many :orders
+
+   validates :nickname, presence: true
+   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナで入力してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+   validates :birthday, presence: true
+   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end

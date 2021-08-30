bb25994f27c52b6c5ab93d26a8129eebcb294892
溝上達朗 <mizogamitatsurou@mizokamitatsurounoiMac.local>
2021-08-31 00:38:25 +0900

    出品した商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,13 +8,18 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+    
   end
   
   private
   
   def item_params
-    params.require(:item).permit(:item_name, :text, :price, :category_id, :status_id, :shipping_burden_id, :prefecture_id, :days_ship_id)
+    params.require(:item).permit(:item_name, :explanation, :text, :price, :category_id, :status_id, :shipping_burden_id, :prefecture_id, :days_ship_id, :image).merge(user_id: current_user.id)
     
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :days_ship, :prefecture, :shipping_burden, :status
 
-  validates :item_name, :explanation, :price, presence: true
+  validates :item_name, :explanation, :price, :image, presence: true
 
   validates :category_id, :days_ship_id, :prefecture_id, :shipping_burden_id, :status_id, numericality: { other_than: 1 , message: "can't be blank"}
 

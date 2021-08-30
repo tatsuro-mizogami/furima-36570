4de53c5b672773e66fee638f962b36be37105ae5
溝上達朗 <mizogamitatsurou@mizokamitatsurounoiMac.local>
2021-08-30 17:41:17 +0900

    ActiveHashの導入と実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_name, :text, :price, :category_id, :status_id, :shipping_burden_id, :prefecture_id, :days_ship_id)
+    
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :days_ship, :prefecture, :shipping_burden, :status
+
+  validates :item_name, :explanation, :price, presence: true
+
+  validates :category_id, :days_ship_id, :prefecture_id, :shipping_burden_id, :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end

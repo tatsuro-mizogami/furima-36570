587bb6851d477c2f6697f8801f3a19f10e3c6f52
溝上達朗 <mizogamitatsurou@mizokamitatsurounoiMac.local>
2021-09-03 10:53:37 +0900

    編集ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @item = Item.all.order('created_at DESC')
   end
@@ -25,10 +27,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :explanation, :text, :price, :category_id, :status_id, :shipping_burden_id,
                                  :prefecture_id, :days_ship_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [ :new, :create, :index, :show, :edit] 
+  resources :items, only: [ :new, :create, :index, :show, :edit, :update] 
 end
